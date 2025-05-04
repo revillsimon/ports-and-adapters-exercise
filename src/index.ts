9233910e5f75acd42111946bbf6c201b67fcc315
Simon Revill <simon.revill@vertumotors.com>
2025-05-04 10:11:50 +0100

    refactor: remove entry point temporarily until needed

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { CounterService } from "./domain/CounterService.js";
-import { LocalStorageCounterRepository } from "./infrastructure/persistence/LocalStorageCounterRepository.js";
-
-const repository = new LocalStorageCounterRepository();
-const service = new CounterService(repository);
-
-console.log("Current count:", service.getCount());
-service.increment();
-console.log("Updated count:", service.getCount());
